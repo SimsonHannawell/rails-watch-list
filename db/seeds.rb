ec64e1619224e09c7e16908580a2ce5196608201
Simson Hannawell <simsonhannawell@gmail.com>
2025-05-08 16:03:17 +0100

    challenge 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman...", poster_url: "...", rating: 6.9)
+Movie.create(title: "The Shawshank Redemption", overview: "Framed...", poster_url: "...", rating: 8.7)
+Movie.create(title: "Titanic", overview: "101-year-old Rose...", poster_url: "...", rating: 7.9)
+Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean...", poster_url: "...", rating: 7.0)
